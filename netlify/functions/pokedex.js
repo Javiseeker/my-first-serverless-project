55360bbb2fde5c733c79353e352fa7aee9ca2ec6
javiseeker <javier.alonso@m9-group.com>
2021-07-25 17:16:16 -0500

    added changes. Trying out a project with just serverless functions

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,9 +1,10 @@
 const fetch = require('node-fetch')
 
-exports.handler = async function () {
+exports.handler = async function (event, context) {
   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
   const response = await fetch(POKE_API)
   const data = await response.json()
+  console.log(event)
   return{
     statusCode: 200,
     body: JSON.stringify(data)
