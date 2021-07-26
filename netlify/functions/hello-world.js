f6450ee08b4d661f8049e4b5e529f6619440ed57
javiseeker <javier.alonso@m9-group.com>
2021-07-25 20:12:56 -0500

    trying another way to set cors

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,4 +1,9 @@
 exports.handler = async function () {
+  const headers = {
+    'Access-Control-Allow-Origin': 'main--my-first-serveless-site-javisito.netlify.app',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+  };
   return {
     statusCode: 200,
     body: JSON.stringify({
